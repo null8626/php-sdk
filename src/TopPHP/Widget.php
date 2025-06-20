a43db30d0bec88076e2bb07379c66f98c4c6e4ad
null8626 <vierofernando9@gmail.com>
2025-06-20 11:08:44 +0700

    feat: use /discord/:type path for widgets

--- src/TopPHP/Widget.php
+++ src/TopPHP/Widget.php
@@ -2,47 +2,56 @@
 
 namespace DBL;
 
+enum WidgetType: string {
+  case DiscordBot = "discord/bot";
+  case DiscordServer = "discord/server";
+}
+
 final class Widget {
   private const BASE_URL = "https://top.gg/api/v1";
 
   /**
    * Generates a large widget URL.
    *
-   * @param   int     $id       The ID.
+   * @param   WidgetType   $ty       The widget type.
+   * @param   int          $id       The ID.
    * @return  string
    */
-  public static function large(int $id): string {
-    return self::BASE_URL . "/widgets/large/$id";
+  public static function large(WidgetType $ty, int $id): string {
+    return self::BASE_URL . "/widgets/large/" . $ty->value . "/$id";
   }
 
   /**
    * Generates a small widget URL for displaying votes.
    *
-   * @param   int     $id       The ID.
+   * @param   WidgetType   $ty       The widget type.
+   * @param   int          $id       The ID.
    * @return  string
    */
-  public static function votes(int $id): string {
-    return self::BASE_URL . "/widgets/small/votes/$id";
+  public static function votes(WidgetType $ty, int $id): string {
+    return self::BASE_URL . "/widgets/small/votes/" . $ty->value . "/$id";
   }
 
   /**
    * Generates a small widget URL for displaying an entity's owner.
    *
-   * @param   int     $id       The ID.
+   * @param   WidgetType   $ty       The widget type.
+   * @param   int          $id       The ID.
    * @return  string
    */
-  public static function owner(int $id): string {
-    return self::BASE_URL . "/widgets/small/owner/$id";
+  public static function owner(WidgetType $ty, int $id): string {
+    return self::BASE_URL . "/widgets/small/owner/" . $ty->value . "/$id";
   }
 
   /**
    * Generates a small widget URL for displaying social stats.
    *
-   * @param   int     $id       The ID.
+   * @param   WidgetType   $ty       The widget type.
+   * @param   int          $id       The ID.
    * @return  string
    */
-  public static function social(int $id): string {
-    return self::BASE_URL . "/widgets/small/social/$id";
+  public static function social(WidgetType $ty, int $id): string {
+    return self::BASE_URL . "/widgets/small/social/" . $ty->value . "/$id";
   }
 }
 
